e2e0075998afb64a8c5cae8226b899bc4c6c7dc6
Ashish jaswal <jaswalashish131@gmail.com>
2023-02-08 00:57:36 +0530

    data added

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/shopDB",{useNewParser:true,useUnifiedTopology:true}).then(()=>console.log("Connection successfull")).catch((err)=>console.log(err));
+mongoose.connect("mongodb://localhost:27017/shopDB",{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>console.log("Connection successfull")).catch((err)=>console.log(err));
+
+const playlistSchema  = new mongoose.Schema({
+
+  id:{
+    type:Number,
+    required:true
+  },
+  name:String,
+  price:Number,
+  active:Boolean,
+  date:{
+    type:Date,
+    default:Date.now
+  }
+})
+
+const Playlist = new mongoose.model("Playlist",playlistSchema);
+
+const playslistone  = new Playlist({
+  id:1,
+  name:"I don't wanna live forever",
+  price:20000,
+  active:true,
+})
+playslistone.save();
